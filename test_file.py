be2007adf70e3174ce8a58e26bec640cb9c53a5f
cbirkho <connor_birkhold@gap.com>
2021-04-12 16:37:58 -0700

    first commit

---
+++ test_file.py
@@ -0,0 +1,11 @@
+class Math:
+
+    def self.__init__(x, y):
+        self.x = x
+        self.y = y
+
+    def add(self):
+        return self.x + self.y
+
+    def subtract(self):
+        return self.x - self.y
